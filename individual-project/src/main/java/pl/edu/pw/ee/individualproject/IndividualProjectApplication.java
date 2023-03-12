2805b04373e49b2b35969dd527a0a850a1e1924e
KubaOrz <orz.kub@wp.pl>
2023-03-12 20:03:33 +0100

    Szkielet backendu

---
+++ individual-project/src/main/java/pl/edu/pw/ee/individualproject/IndividualProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.ee.individualproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividualProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndividualProjectApplication.class, args);
+	}
+
+}
